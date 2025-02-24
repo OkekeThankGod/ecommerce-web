ec263126a5fa52363829f52ec59755727ca840bb
OkekeThankGod <okekethankgod192@gmail.com.com>
2025-02-24 12:38:42 -1100

    new

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import ShopContextProvider from './context/ShopContext.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom'; // Import HashRouter
+import './index.css';
+import App from './App.jsx';
+import ShopContextProvider from './context/ShopContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ShopContextProvider >
-    <App />
-    </ShopContextProvider >
-  </StrictMode>,
-)
+    <ShopContextProvider>
+      <HashRouter> {/* Wrap App inside HashRouter */}
+        <App />
+      </HashRouter>
+    </ShopContextProvider>
+  </StrictMode>
+);
+
